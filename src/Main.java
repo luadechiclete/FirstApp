edb73fecb688eb405d3be47f9fa8ed77a728b044
Luana <santosmeiraluana@gmail.com>
2023-01-05 20:18:42 -0300

    comentários

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
+    /*
+    This is a the first comment line
+    This is the second comment line
+     */ //This is a comment
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("First line from app"); // This is a line comment
+        //System.out.println("Second line from app");
+        System.out.println(/*"Third line from app"*/);
     }
 }
